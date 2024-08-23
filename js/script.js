c29c34dee21ce44bc9fec00ea58884181390c0eb
afzbehroz <145428759+afzbehroz@users.noreply.github.com>
2024-08-23 11:41:14 +0200

    login html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 // Code here!
+// E1
+document.querySelectorAll("h3")[0].innerText = "Potato";
+
+// E2
+document.querySelectorAll("a")[0].innerText = "Start";
+
+// E3
+// Step 1: Select the footer element
+document.querySelectorAll("footer a")[2].innerText = "Mail us";
+
+// E4
+document.querySelector("button").style.backgroundColor = "darkblue";
+
+// E5
+document.querySelectorAll("main p")[1].innerText = "This is the change";
+
+// E6
+document.querySelector("article figure").style.backgroundColor = "red";
+
+// E7
+document.querySelectorAll("footer p")[0].innerText = "Stockholm Globen";
+
+ // E8
+ document.querySelector("button").innnerText = "darkblue";
+
